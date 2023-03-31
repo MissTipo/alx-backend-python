ae72d22e92b23c6cdfee18862c3668f5807b0a90
MissTipo <dorine.a.tipo@gmail.com>
2023-03-31 05:47:27 +0300

    Add docstrings

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,11 @@
 from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+    that takes a float multiplier as argument and returns a function
+    that multiplies a float by multiplier
+    '''
     def multipliers(n: float) -> float:
+        '''multiplies a float by multiplier'''
         return n * multiplier
     return multipliers
