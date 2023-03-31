65be8eea272714f5e3ef10f1195af15ed5bacae1
MissTipo <dorine.a.tipo@gmail.com>
2023-03-31 05:32:26 +0300

    fix indentation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,5 +4,5 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-        '''Takes a list of integers and floats and returns their float sum'''
-        return sum(mxd_lst)
+    '''Takes a list of integers and floats and returns their float sum'''
+    return sum(mxd_lst)
