02b414317d61c3858935101d1923cd1db6ad1ab3
MissTipo <dorine.a.tipo@gmail.com>
2023-03-31 05:47:59 +0300

    Add docstrings

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 '''Complex types - functions'''
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     '''
     that takes a float multiplier as argument and returns a function
