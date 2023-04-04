1b1941f949fcc3c8017bc7d26ed3a8514d8d3e3e
MissTipo <dorine.a.tipo@gmail.com>
2023-04-04 14:08:38 +0300

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Async Generator'''
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+#async def async_generator(): 
+    '''A coreroutine that takes no argument'''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
