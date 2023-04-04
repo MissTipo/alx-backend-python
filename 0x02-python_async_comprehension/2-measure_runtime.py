acc15495229d51020a0fe5a1c6c947698d5eb1b8
MissTipo <dorine.a.tipo@gmail.com>
2023-04-04 14:20:31 +0300

    Measure runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Run time for four parallel comprehensions'''
+
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    ''''''
+    start = time.perf_counter()
+    task = [asyncio.create_task(async_comprehension()) for i in range(4)]
+    result = await asyncio.gather(*task)
+    end = time.perf_counter()
+    total_time = end - start
+    return total_time
