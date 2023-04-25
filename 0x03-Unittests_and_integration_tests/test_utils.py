f0e5d9c138028b01858ab61325e847ae7456b902
MissTipo <dorine.a.tipo@gmail.com>
2023-04-25 05:59:32 +0300

    memoize a method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -47,3 +47,23 @@ class TestGetJson(unittest.TestCase):
             mock_get.return_value.json.return_value = test_payload
             response = get_json(test_url)
             self.assertEqual(response, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """"""
+    def test_memoize(self):
+        """"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with mock.patch.object(TestClass, 'a_method') as mock_a_method:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock_a_method.assert_called_once()
